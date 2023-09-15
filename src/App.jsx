b3636d65ea39da4c3af3b17afeba6c116e9f2b3d
Behruz01 <ibragimovbehruz822@gmail.com>
2023-09-15 15:39:44 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl text-blue-900 font-bold">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
