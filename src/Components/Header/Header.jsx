4d7e3b3b874cfcc17907b771894db7fb2b9a1d07
madrimovs <sardormadrimov516@gmail.com>
2023-09-16 20:10:41 +0500

    addHeaderSwiper

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./header.css";
 
-// import bg_hero from "../../Assets/bg_hero.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import "./styles.css";
+
+// import required modules
+import { Pagination, Autoplay } from "swiper/modules";
+
 import Navbar from "./Navbar/Navbar";
 import logoHeader from "../../Assets/logoHero.svg";
 
@@ -12,14 +22,14 @@ const Header = () => {
             <div className="containr">
                 <Navbar />
 
-                <div className="mt-16 text-white absolute flex items-center justify-center containr">
+                <div className="mt-16 text-white flex items-center justify-center containr ">
                     <div
                         id="headerEduLogo"
                         className="border-2 mr-28 border-white px-10 py-14 backdrop-blur-lg"
                     >
                         <img className="w-48 h-48" src={logoHeader} alt="" />
                     </div>
-                    <div className="w-1/2 header_content">
+                    <div className="w-1/2 relative header_content">
                         <h1 className="text-3xl mb-12 font-bold">
                             O‘zbekiston Respublikasi maktabgacha va maktab ta'limi
                             vazirligi
@@ -29,6 +39,29 @@ const Header = () => {
                             maktab qiladigan kuch esa o‘qituvchilardir
                         </p>
                         <p className="mt-5 font-bold">Shavkat Mirziyoyev</p>
+                    </div>
+                </div>
+
+                <div className="mt-28">
+                    <div className="w-1/2 header_swiper right-32 absolute">
+                        <Swiper
+                            slidesPerView={3}
+                            spaceBetween={30}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            autoplay={{ delay: 2000 }}
+                            modules={[Autoplay]}
+                            className="mySwiper text-white text-xl"
+                        >
+                            <SwiperSlide>
+                                <p>10 104 Maktab</p>
+                            </SwiperSlide>
+                            <SwiperSlide>6mln O'quvchilar</SwiperSlide>
+                            <SwiperSlide>506 618 O'qituvchilar</SwiperSlide>
+                            <SwiperSlide>219 Barkamol alvlod maktablari</SwiperSlide>
+                            <SwiperSlide>3 Mehribonlik uylari</SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
             </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 // import Layout from "../Components/Layout/Layout";
 import Header from "../Components/Header/Header";
-import Main from "../Components/Main/Main";
+// import Main from "../Components/Main/Main";
 import Footer from "../Components/Footer/Footer";
 
 const Home = () => {
     return (
         <>
             <Header />
-            <Main />
+            {/* <Main /> */}
             <Footer />
         </>
     );
