082421abfb5565a92665e21c2ace6e6c2e992191
madrimovs <sardormadrimov516@gmail.com>
2023-09-18 16:08:18 +0500

    setMainCards

--- src/Components/Main/MainContent/MainCards/MainCards.jsx
+++ src/Components/Main/MainContent/MainCards/MainCards.jsx
@@ -4,7 +4,7 @@ import mainCardIcon1 from "../../../../Assets/mainCardIcon1.jpg";
 const MainCards = () => {
     return (
         <div className="mt-10 flex items-center justify-around flex-row flex-wrap">
-            <div className="mb-10 relative hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
+            <div className="mb-10 relative group hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
                 <img className="mx-auto" src={mainCardIcon1} alt="" />
                 <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
                 <div className="text-md">
@@ -13,11 +13,11 @@ const MainCards = () => {
                     <p>Smartland</p>
                     <p>To'garaklar</p>
                 </div>
-                <button className="absolute bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
+                <button className="absolute group-hover:bg-[#f0f220] group-hover:text-[#4c4083] font-bold transition delay-75 bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
                     Batafsil
                 </button>
             </div>
-            <div className="mb-10 relative hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
+            <div className="mb-10 relative group hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
                 <img className="mx-auto" src={mainCardIcon1} alt="" />
                 <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
                 <div className="text-md">
@@ -26,11 +26,11 @@ const MainCards = () => {
                     <p>Smartland</p>
                     <p>To'garaklar</p>
                 </div>
-                <button className="absolute bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
+                <button className="absolute group-hover:bg-[#f0f220] group-hover:text-[#4c4083] font-bold transition delay-75 bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
                     Batafsil
                 </button>
             </div>
-            <div className="mb-10 relative hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
+            <div className="mb-10 relative group hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
                 <img className="mx-auto" src={mainCardIcon1} alt="" />
                 <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
                 <div className="text-md">
@@ -39,11 +39,11 @@ const MainCards = () => {
                     <p>Smartland</p>
                     <p>To'garaklar</p>
                 </div>
-                <button className="absolute bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
+                <button className="absolute group-hover:bg-[#f0f220] group-hover:text-[#4c4083] font-bold transition delay-75 bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
                     Batafsil
                 </button>
             </div>
-            <div className="mb-10 relative hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
+            <div className="mb-10 relative group hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
                 <img className="mx-auto" src={mainCardIcon1} alt="" />
                 <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
                 <div className="text-md">
@@ -52,7 +52,7 @@ const MainCards = () => {
                     <p>Smartland</p>
                     <p>To'garaklar</p>
                 </div>
-                <button className="absolute bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
+                <button className="absolute group-hover:bg-[#f0f220] group-hover:text-[#4c4083] font-bold transition delay-75 bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
                     Batafsil
                 </button>
             </div>
