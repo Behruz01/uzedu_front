d00e6abc4c05642a201191233ef4b59638f6c443
madrimovs <sardormadrimov516@gmail.com>
2023-09-18 16:03:07 +0500

    addMainCards

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import MainNav from "./MainContent/MainNav";
 import "./main.css";
 import MainAdvertising from "./MainContent/MainAdvertising";
+import MainCards from "./MainContent/MainCards/MainCards";
 
 const Main = () => {
     return (
         <div className="containr">
             <MainNav />
             <MainAdvertising />
+            <MainCards />
         </div>
     );
 };
---
+++ src/Components/Main/MainContent/MainCards/MainCards.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import mainCardIcon1 from "../../../../Assets/mainCardIcon1.jpg";
+
+const MainCards = () => {
+    return (
+        <div className="mt-10 flex items-center justify-around flex-row flex-wrap">
+            <div className="mb-10 relative hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
+                <img className="mx-auto" src={mainCardIcon1} alt="" />
+                <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
+                <div className="text-md">
+                    <p>Darsliklar</p>
+                    <p>Videodarsliklar</p>
+                    <p>Smartland</p>
+                    <p>To'garaklar</p>
+                </div>
+                <button className="absolute bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
+                    Batafsil
+                </button>
+            </div>
+            <div className="mb-10 relative hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
+                <img className="mx-auto" src={mainCardIcon1} alt="" />
+                <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
+                <div className="text-md">
+                    <p>Darsliklar</p>
+                    <p>Videodarsliklar</p>
+                    <p>Smartland</p>
+                    <p>To'garaklar</p>
+                </div>
+                <button className="absolute bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
+                    Batafsil
+                </button>
+            </div>
+            <div className="mb-10 relative hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
+                <img className="mx-auto" src={mainCardIcon1} alt="" />
+                <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
+                <div className="text-md">
+                    <p>Darsliklar</p>
+                    <p>Videodarsliklar</p>
+                    <p>Smartland</p>
+                    <p>To'garaklar</p>
+                </div>
+                <button className="absolute bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
+                    Batafsil
+                </button>
+            </div>
+            <div className="mb-10 relative hover:drop-shadow-2xl shadow-2xl w-80 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
+                <img className="mx-auto" src={mainCardIcon1} alt="" />
+                <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
+                <div className="text-md">
+                    <p>Darsliklar</p>
+                    <p>Videodarsliklar</p>
+                    <p>Smartland</p>
+                    <p>To'garaklar</p>
+                </div>
+                <button className="absolute bottom-0 left-0 right-0 w-1/2 mx-auto mb-7 bg-[#4c4083] text-white px-12 py-2 rounded-lg ">
+                    Batafsil
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default MainCards;
