1f5ece4eaf58186dba127fe6ade4f36b3a54001f
madrimovs <sardormadrimov516@gmail.com>
2023-09-19 16:15:06 +0500

    setMainCards

--- src/Components/Main/MainContent/MainCards/MainCards.jsx
+++ src/Components/Main/MainContent/MainCards/MainCards.jsx
@@ -6,7 +6,7 @@ const MainCards = () => {
         <div className="mt-10 flex items-center containr justify-around gap-2 flex-row flex-wrap">
             <div className="mb-10 relative group hover:drop-shadow-2xl shadow-2xl w-96 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
                 <img className="mx-auto" src={mainCardIcon1} alt="" />
-                <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
+                <h2 className="text-2xl font-bold my-5">O'QUVCHILARGA</h2>
                 <div className="text-md">
                     <p>Darsliklar</p>
                     <p>Videodarsliklar</p>
@@ -19,7 +19,7 @@ const MainCards = () => {
             </div>
             <div className="mb-10 relative group hover:drop-shadow-2xl shadow-2xl w-96 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
                 <img className="mx-auto" src={mainCardIcon1} alt="" />
-                <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
+                <h2 className="text-2xl font-bold my-5">OTA-ONALARGA</h2>
                 <div className="text-md">
                     <p>Darsliklar</p>
                     <p>Videodarsliklar</p>
@@ -32,7 +32,7 @@ const MainCards = () => {
             </div>
             <div className="mb-10 relative group hover:drop-shadow-2xl shadow-2xl w-96 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
                 <img className="mx-auto" src={mainCardIcon1} alt="" />
-                <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
+                <h2 className="text-2xl font-bold my-5">O'QITUVCHILARGA</h2>
                 <div className="text-md">
                     <p>Darsliklar</p>
                     <p>Videodarsliklar</p>
@@ -45,7 +45,7 @@ const MainCards = () => {
             </div>
             <div className="mb-10 relative group hover:drop-shadow-2xl shadow-2xl w-96 h-[450px] ease-in-out duration-300 cursor-pointer bg-white rounded-3xl p-10 text-center">
                 <img className="mx-auto" src={mainCardIcon1} alt="" />
-                <h2 className="text-2xl font-bold my-5">O'quvchilarga</h2>
+                <h2 className="text-2xl font-bold my-5">MAKTABLARGA</h2>
                 <div className="text-md">
                     <p>Darsliklar</p>
                     <p>Videodarsliklar</p>
